8cb6d8075defd44719cc4db83111a430df4a2d32
Oliwier <oliwierrosiak07@gmail.com>
2025-07-25 13:11:50 +0200

    test

--- controller/controller.js
+++ controller/controller.js
@@ -8,12 +8,15 @@ class Controller
         {
             const message = await transporter.sendMail({
                 from:"maksiufotomailer@gmail.com",
-                to: "maxbookowski@gmail.com", 
+                to: "oliwierrosiak07@gmail.com", 
                 subject: "Nowa Wiadomość",
                 text: `Nowa wiadomość od ${req.body.email}: ${req.body.message}`, 
                 html: `<b>Nowa wiadomość od ${req.body.email}</b><br><p>${req.body.message}</p>`, 
             })
-            res.sendStatus(200)
+            setTimeout(() => {
+                res.sendStatus(200)
+                
+            }, 3000);
         }
         catch(ex)
         {
@@ -27,12 +30,15 @@ class Controller
         {
             const message = await transporter.sendMail({
                 from:"maksiufotomailer@gmail.com",
-                to: "maxbookowski@gmail.com", 
+                to: "oliwierrosiak07@gmail.com", 
                 subject: "Nowy Termin Sesji",
                 text: `Użytkownik ${req.body.email} wybrał swoją sesję zdjeciową. Kategoria: ${req.body.category}, data: ${req.body.date}`, 
                 html: `<p>Użytkownik ${req.body.email} wybrał swoją sesję zdjeciową. Kategoria: ${req.body.category}, data: ${req.body.date}</p>`, 
             })
-            res.sendStatus(200)
+            setTimeout(() => {
+                res.sendStatus(200)
+                
+            }, 3000);
         }
         catch(ex)
         {
